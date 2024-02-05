2377320074addec7aa6425532b3b1280fb4ad0d1
deepika2007 <deepika200799@gmail.com>
2024-02-05 23:54:55 +0530

    First flow code

---
+++ src/schema/todos/todo.schema.ts
@@ -0,0 +1,18 @@
+// import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+// import { HydratedDocument } from 'mongoose';
+
+// export type TodoDocument = HydratedDocument<Todo>;
+
+// @Schema()
+// export class Todo {
+//   @Prop()
+//   name: string;
+
+//   @Prop()
+//   age: number;
+
+//   @Prop()
+//   breed: string;
+// }
+
+// export const TodoSchema = SchemaFactory.createForClass(Todo);
